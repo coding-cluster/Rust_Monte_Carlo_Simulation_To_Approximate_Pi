fd528ceb0108b03d7da25bcc9b8c370095a3de36
Coding Cluster <108909645+coding-cluster@users.noreply.github.com>
2025-11-29 23:38:13 -0600

    v1.0.1 - Optimised, Added a Progress Bar

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         .read_line(&mut n)
         .expect("Failed to read input");
     
-    let n: i32 = match n.trim().parse() {
+    let n: usize = match n.trim().parse() {
         Ok(num) => {
             println!("{}", "Successful parsing".green());
             num
@@ -23,11 +23,28 @@ fn main() {
         }
     };
     
-    let mut inside: i32 = 0;
+    let mut inside: usize = 0;
+    let mut rng = rand::rng();
+    let bar_width = 50;
     
     for i in 1..=n {
-        let x: f32 = rand::rng().random_range(-1.0..=1.0);
-        let y: f32 = rand::rng().random_range(-1.0..=1.0);
+        let x: f32 = rng.random_range(-1.0..=1.0);
+        let y: f32 = rng.random_range(-1.0..=1.0);
+        
+        if i % (n / bar_width) == 0 {
+            let progress = i as f32 / n as f32;
+            let filled = (progress * bar_width as f32) as usize;
+            
+            let bar: String = format!(
+                "\r[{}{}] {:>3}%",
+                "#".repeat(filled),
+                ".".repeat(bar_width - filled),
+                (progress * 100.0) as u32,
+            );
+            
+            print!("{}", bar);
+            io::stdout().flush().unwrap();
+        }
         
         if x*x + y*y <= 1.0 {
             inside += 1;
@@ -35,7 +52,7 @@ fn main() {
     }
     
     let pi: f32 = 4.0 * (inside as f32 / n as f32);
-    println!("The approximation of pi is: {}", pi);
+    println!("\nThe approximation of pi is: {}", pi);
 }
 
 
